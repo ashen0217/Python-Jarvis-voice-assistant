da035b29169b4f5c903fe1b3a670db3c6401e0c3
Ashen Bamunusingha <ashen0217@gmail.com>
2025-06-19 22:20:21 +0530

    fg

--- Jarvis/core/tts.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def speak(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
-if __name__ == '__main__':
-    speak("Hello, this is Jarvis speaking.")
